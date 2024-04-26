299fa8c6cc5726ed549b8d3af3ea73f33462108f
Alexey Kovrizhkin <lekovr@gmail.com>
2024-04-26 23:22:03 +0300

    fix: gitflow & test

--- static/static_test.go
+++ static/static_test.go
@@ -11,7 +11,7 @@ func TestNewEmbed(t *testing.T) {
 	got, err := static.New("tmpl/")
 	ass.NotZero(t, got, "FS not nil")
 	ass.NoError(t, err, "New success")
-	f, err := got.Open("header.html")
+	f, err := got.Open("header.gohtml")
 	errC := f.Close()
 	ass.NoError(t, err, "Open success")
 	ass.NoError(t, errC, "Close success")
